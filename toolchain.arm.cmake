763dec6026baceda04b0030bc31dd18091778c0c
KHLee <alexeibatic@gmail.com>
2018-02-08 16:53:33 +0900

    change path to MIICA

--- toolchain.arm.cmake
+++ toolchain.arm.cmake
@@ -1,10 +1,10 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_PROCESSOR arm)
 
-SET(COMPILER_ROOT "E:/Work/Development/UTOPIIA_Resource/Toolchaine/applied/mingw/bin/arm-linux-gnueabihf-")
+SET(COMPILER_ROOT "E:/Work/Development/MIICA_Resource/Toolchaine/applied/mingw/bin/arm-linux-gnueabihf-")
 
 SET(CMAKE_C_COMPILER ${COMPILER_ROOT}gcc.exe)
-SET(CMAKE_CXX_COMPILER E:/Work/Development/UTOPIIA_Package/UTOPIIA/Resources/build/ARM_Linux/gcc-linaro-7.1.1-2017.08-i686-mingw32_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-g++.exe)
+SET(CMAKE_CXX_COMPILER E:/Work/Development/MIICA_Package/MIICA/Resources/build/ARM_Linux/gcc-linaro-7.1.1-2017.08-i686-mingw32_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-g++.exe)
 SET(CMAKE_LINKER ${COMPILER_ROOT}ld.exe)
 SET(CMAKE_NM ${COMPILER_ROOT}nm.exe)
 SET(CMAKE_OBJCOPY ${COMPILER_ROOT}objcopy.exe)
